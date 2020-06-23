357144cc51cf079cd5757387dda6cbef9aa2dcbc
youzhi.gao <youzhi.gao@ikang.com>
2020-06-23 11:02:57 +0800

    正确停止线程interrupt

---
+++ src/main/java/com/haochang/thread/Consumer.java
@@ -0,0 +1,24 @@
+package com.haochang.thread;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @description: 描述：消费者
+ * @author: youzhi.gao
+ * @date: 2020-06-23 10:23
+ */
+public class Consumer {
+    BlockingQueue storage;
+
+    public Consumer(BlockingQueue storage) {
+        this.storage = storage;
+    }
+
+    public boolean needMoreNums(){
+        if(Math.random() > 0.97){
+            return false;
+        }
+
+        return true;
+    }
+}
